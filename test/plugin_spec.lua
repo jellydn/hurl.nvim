180b42d6326b54ce084fa317140d797f35a533d2
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-25 09:40:23 +0800

    test: add config test

--- test/plugin_spec.lua
+++ test/plugin_spec.lua
@@ -1,7 +1,19 @@
-local hurl = require('hurl')
-
 describe('Hurl.nvim plugin', function()
   it('should be able to load', function()
+    local hurl = require('hurl')
     assert.truthy(hurl)
+
+    assert.are.same('popup', _HURL_CFG.mode)
+    assert.are.same(false, _HURL_CFG.debug)
+  end)
+
+  it('should be able parse the configuration file', function()
+    require('hurl').setup({
+      debug = true,
+      mode = 'split',
+    })
+
+    assert.are.same('split', _HURL_CFG.mode)
+    assert.are.same(true, _HURL_CFG.debug)
   end)
 end)
