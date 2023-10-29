61c23d602c479560ebe4bbf688b7d78a1863942f
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-30 00:20:52 +0800

    chore: add note about global config for plugin

--- lua/hurl/init.lua
+++ lua/hurl/init.lua
@@ -1,8 +1,10 @@
---- Global configuration for hurl.nvim
-_HURL_CFG = {
+--- Default configuration for hurl.nvim
+local default_config = {
   debug = false,
   mode = 'split',
 }
+--- Global configuration for entire plugin, easy to access from anywhere
+_HURL_CFG = default_config
 local M = {}
 
 --- Setup hurl.nvim
@@ -10,7 +12,7 @@ local M = {}
 --       - debug: (boolean | nil) default: false.
 --       - mode: ('popup' | 'split') default: popup.
 function M.setup(options)
-  _HURL_CFG = vim.tbl_extend('force', _HURL_CFG, options or {})
+  _HURL_CFG = vim.tbl_extend('force', _HURL_CFG, options or default_config)
 
   require('hurl.main').setup()
 end
